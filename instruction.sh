dc6b361543376d77d721ce5204fccfae80fae81e
Ubuntu <ubuntu@ip-172-31-3-50.us-east-2.compute.internal>
2023-03-24 01:03:53 +0000

    Created for bashscripting

---
+++ instruction.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#OWNER: IFEOLUWA BALOGUN
+#DATE: March 23rd 2023
+#Description: Introduction into automation using bsh shell scripting. This file will contain various linux commands to test run bash scripting
+
+
+#greetings from the machine
+echo "Hi there! I hope you are doing well today."
+sleep 5
+clear
+
+
+#This command is meant to reveal the current user signed into the machine
+echo "You are the user called"
+whoami
+sleep 5
+clear
+
+
+
+
+#This command reveals the present working directory
+echo "You are currently working in this directory""
+pwd
+sleep 5
+clear
